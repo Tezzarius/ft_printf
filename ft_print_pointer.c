2c985e67e0fea0fa6043e4d5dd945c11c36f9597
bschwarz <bschwarz@student.42vienna.com>
2025-05-16 14:19:43 +0200

    finish

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,16 +6,26 @@
 /*   By: bschwarz <bschwarz@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 10:32:57 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/05/16 13:35:23 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/05/16 14:19:28 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
 
+static void	print_hexlong(unsigned long n, const char x)
+{
+	char	*base;
+
+	base = "0123456789abcdef";
+	if (n >= 16)
+		print_hexlong(n / 16, x);
+	ft_print_char(base[n % 16]);
+}
+
 int	ft_print_pointer(void *ptr)
 {
-	unsigned long	c;
+	unsigned long	n;
 	int				i;
 
 	if (!ptr)
@@ -23,9 +33,15 @@ int	ft_print_pointer(void *ptr)
 		write(1, "(nil)", 5);
 		return (5);
 	}
-	c = (unsigned long) ptr;
+	n = (unsigned long) ptr;
 	write(1, "0x", 2);
-	i = ft_print_hex(c, 'x');
+	print_hexlong(n, 'x');
+	i = 0;
+	while (n > 0)
+	{
+		n = n / 16;
+		i++;
+	}
 	i += 2;
 	return (i);
 }
