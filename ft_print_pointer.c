f4e2a988ef2af5b85b28f7166047347a3aa5de99
bschwarz <bschwarz@student.42vienna.com>
2025-05-16 13:36:31 +0200

    in progress

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: bschwarz <bschwarz@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 10:32:57 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/05/16 10:42:38 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/05/16 13:35:23 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int	ft_print_pointer(void *ptr)
 {
 	unsigned long	c;
+	int				i;
 
 	if (!ptr)
 	{
@@ -24,8 +25,9 @@ int	ft_print_pointer(void *ptr)
 	}
 	c = (unsigned long) ptr;
 	write(1, "0x", 2);
-	ft_print_hex(c, 'x');
-	return (0);
+	i = ft_print_hex(c, 'x');
+	i += 2;
+	return (i);
 }
 
 /* #include <stdio.h>
